a4b14b443665c68c1f5dd7707c3cd3212a809817
Owen Medina <owensmedi@gmail.com>
2019-02-27 06:46:55 +0800

    Created Xcode project and created basic UI for Search Recipes page

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Owen Medina on 26/02/2019.
+//  Copyright © 2019 Owen Medina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
